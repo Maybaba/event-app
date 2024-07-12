0ecd102d7504039caca40f33d096ce3a11b75983
Maybaba <sinyunjong@gmail.com>
2024-07-13 08:31:12 +0900

    password input

--- src/components/auth/VerificationInput.js
+++ src/components/auth/VerificationInput.js
@@ -11,8 +11,11 @@ const VerificationInput = ({ email }) => {
   // 입력한 인증코드를 저장
   const [codes, setCodes] = useState(Array(4).fill(''));
 
-  //에러 메시지 저장
+  // 에러메시지 저장
   const [error, setError] = useState('');
+
+  // 타이머 시간
+  const [timer, setTimer] = useState(300);
 
   // 다음 칸으로 포커스를 이동하는 함수
   const focusNextInput = (index) => {
@@ -24,26 +27,31 @@ const VerificationInput = ({ email }) => {
 
   // 서버에 검증요청 보내기
   const verifyCode = debounce(async (code) => {
-    // console.log('요청 전송!: ', code);
+    console.log('요청 전송!: ', code);
 
     const response = await fetch(`${AUTH_URL}/code?email=${email}&code=${code}`);
     const flag = await response.json();
 
     console.log('코드검증: ', flag);
-    //검증에 실패했을때
-    if(!flag) {
-      setError('유효하지 않거나 만료된 코드입니다. 다시 인증코드를  확인해주세요');
-      //기존 인증코드 상태값 비우기
+    // 검증에 실패했을 때
+    if (!flag) {
+      setError('유효하지 않거나 만료된 코드입니다. 인증코드를 재발송합니다.');
+      // 기존 인증코드 상태값 비우기
       setCodes(Array(4).fill(''));
+
+      inputsRef.current[0].focus();
+      return;
     }
+
+    //검증 성공 시 
+    
 
   }, 1500);
 
   const changeHandler = (index, inputValue) => {
 
-    const updatedCodes = [...codes ]; //처음 입력하기 전의 값
-    updatedCodes[index -1] = inputValue; 
-
+    const updatedCodes = [ ...codes ];
+    updatedCodes[index - 1] = inputValue;
     console.log(updatedCodes);
 
     // codes변수에 입력한 숫자 담아놓기
@@ -66,6 +74,18 @@ const VerificationInput = ({ email }) => {
   useEffect(() => {
     // 처음엔 첫번째 칸에 포커싱
     inputsRef.current[0].focus();
+
+    // 타이머 설정
+    const intervalId = setInterval(() => {
+      setTimer(prevTime => prevTime > 0 ? prevTime - 1 : 0);
+    }, 1000);
+
+    // 타이머 리셋
+    return () => {
+      clearInterval(intervalId);
+    };
+
+  
   }, []);
 
   return (
@@ -81,14 +101,16 @@ const VerificationInput = ({ email }) => {
                 className={styles.codeInput}
                 maxLength={1}
                 onChange={(e) => changeHandler(index + 1, e.target.value)}
-                value={codes[index]} //한글자 입력할 떄마다 체이닞 핸들러에서 코드가 업데이트 되는데
-                //각 칸이 연결되는 밸류값이 각각 리셋된다. 
+                value={codes[index]}
               />
           ))
         }
+      </div>
+      <div className={styles.timer}>
+        {`${'0' + Math.floor(timer / 60)}:${('0' + (timer % 60)).slice(-2)}`}
 
       </div>
-      {error && <p className={styles.errorMessage}>{error}</p>}
+      { error && <p className={styles.errorMessage}>{error}</p> }
     </>
   );
 };
